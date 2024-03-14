bb8773c9a90c90618f159038028c4162538fbc5a
alicia-koh <alicia.koh@data4life-asia.care>
2024-03-14 09:41:39 +0800

    Update flow.py

--- datamodel_plugin/flow.py
+++ datamodel_plugin/flow.py
@@ -5,19 +5,19 @@ import importlib
 
 
 @flow(log_prints=True, task_runner=SequentialTaskRunner)
-def create_ms_datamodel(options: createDataModelType):
+def create_datamodel(options: createDataModelType):
     dbsvc_module = importlib.import_module('d2e_dbsvc')
     dbsvc_module.create_datamodel(options)
 
 
 @flow(log_prints=True, task_runner=SequentialTaskRunner)
-def update_ms_datamodel(options: updateDataModelType):
+def update_datamodel(options: updateDataModelType):
     dbsvc_module = importlib.import_module('d2e_dbsvc')
     dbsvc_module.update_datamodel(options)
 
     
 @flow(log_prints=True, task_runner=SequentialTaskRunner)
-def create_update_ms_datamodel(options: dataModelType):
+def create_update_datamodel(options: dataModelType):
     dbsvc_module = importlib.import_module('d2e_dbsvc')
     if options.flow_action_type == flowActionType.CREATE_DATA_MODEL:
         dbsvc_module.create_datamodel(options)
