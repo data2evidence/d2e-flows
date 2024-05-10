8b8ff8432f1ae05d0be95f7e18882442b3dd594c
alicia-koh <alicia.koh@data4life-asia.care>
2024-05-10 14:49:09 +0800

    update pg eav tables to set varchar length

--- datamodel_plugin/db/migrations/postgres/changesets/medical-imaging/V1.0.0.0.1_create_eav_tables.sql
+++ datamodel_plugin/db/migrations/postgres/changesets/medical-imaging/V1.0.0.0.1_create_eav_tables.sql
@@ -7,15 +7,15 @@ CREATE TABLE dicom_image_metadata(
     metadata_date date NOT NULL,
     metadata_datetime timestamp,
     data_element_tag integer NOT NULL,
-    data_element_source_name varchar NOT NULL,
-    data_element_source_tag varchar NOT NULL,
-    data_element_source_group_number varchar NOT NULL,
-    data_element_source_value_representation varchar NOT NULL,
+    data_element_source_name varchar(255) NOT NULL,
+    data_element_source_tag varchar(255) NOT NULL,
+    data_element_source_group_number varchar(255) NOT NULL,
+    data_element_source_value_representation varchar(255) NOT NULL,
     is_sequence boolean NOT NULL,
     parent_sequence_id integer,
     value_as_integer integer,
     value_as_numeric numeric,
-    value_as_string varchar,
+    value_as_string varchar(5000),
     value_as_datetime timestamp,
     value_as_text text,
     data_element_source_value text NOT NULL,
@@ -24,12 +24,12 @@ CREATE TABLE dicom_image_metadata(
 
 
 CREATE TABLE dicom_data_element(
-    data_element_tag varchar NOT NULL,
-    data_element_name varchar NOT NULL,
-    data_element_keyword varchar NOT NULL,
-    value_representation varchar NOT NULL,
-    value_multiplicity varchar NOT NULL,
-    delimiter varchar,
+    data_element_tag varchar(255) NOT NULL,
+    data_element_name varchar(255) NOT NULL,
+    data_element_keyword varchar(255) NOT NULL,
+    value_representation varchar(255) NOT NULL,
+    value_multiplicity varchar(255) NOT NULL,
+    delimiter varchar(255),
     deprecated boolean
 );
 
