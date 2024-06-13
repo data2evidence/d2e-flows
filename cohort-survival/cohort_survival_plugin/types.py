7628feb7fc06b942b6ac49733b95e0077267f1e1
Jerome Ng <jerome.ng@data4life-asia.care>
2024-06-14 00:06:03 +0800

    update

--- cohort-survival/cohort_survival_plugin/types.py
+++ cohort-survival/cohort_survival_plugin/types.py
@@ -4,5 +4,5 @@ from pydantic import BaseModel
 class cohortSurvivalOptionsType(BaseModel):
     database_code: str
     schema_name: str
-    target_cohort_definition_id: str
-    outcome_cohort_definition_id: str
+    target_cohort_definition_id: int
+    outcome_cohort_definition_id: int
