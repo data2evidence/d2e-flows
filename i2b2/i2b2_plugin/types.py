ae09c3cff9d3c7d7e60639b6e0d1b3fdd6b59280
alicia-koh <110385419+alicia-koh@users.noreply.github.com>
2024-06-14 10:32:29 +0800

    Set load data param to optional

--- i2b2/i2b2_plugin/types.py
+++ i2b2/i2b2_plugin/types.py
@@ -1,8 +1,9 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class i2b2PluginType(BaseModel):
     database_code: str
     schema_name: str
     tag_name: str
-    load_data: bool
+    load_data: Optional[bool]
     
