8868456f39c80187895413421a988fbc344e3437
jerome-ng <107386741+jerome-ng@users.noreply.github.com>
2024-06-28 11:14:11 +0800

    243 add kaplan meier chart (#20)
    * Add flow for cohort survival

---
+++ cohort-survival/cohort_survival_plugin/flow.py
@@ -0,0 +1,38 @@
+import os
+import sys
+import importlib
+from prefect.task_runners import SequentialTaskRunner
+from prefect import flow
+from prefect_shell import ShellOperation
+from cohort_survival_plugin.types import cohortSurvivalOptionsType
+
+
+def setup_plugin():
+    # Setup plugin by adding path to python flow source so that modules from app/pysrc in dataflow-gen-agent container can be imported dynamically
+    sys.path.append("/app/pysrc")
+    r_libs_user_directory = os.getenv("R_LIBS_USER")
+    # force=TRUE for fresh install everytime flow is run
+    if r_libs_user_directory:
+        ShellOperation(
+            commands=[
+                f"Rscript -e \"remotes::install_version('snakecase', quiet=FALSE, upgrade='never', force=TRUE, dependencies=TRUE, repos='https://cloud.r-project.org', lib='{r_libs_user_directory}')\"",
+                f"Rscript -e \"remotes::install_version('broom', quiet=FALSE, upgrade='never', force=TRUE, dependencies=TRUE, repos='https://cloud.r-project.org', lib='{r_libs_user_directory}')\"",
+                f"Rscript -e \"remotes::install_version('visOmopResults', quiet=FALSE, upgrade='never', force=TRUE, dependencies=TRUE, repos='https://cloud.r-project.org', lib='{r_libs_user_directory}')\"",
+                f"Rscript -e \"remotes::install_version('dbplyr', version='2.4.0', quiet=FALSE, upgrade='never', force=TRUE, dependencies=FALSE, repos='https://cloud.r-project.org', lib='{r_libs_user_directory}')\"",
+                f"Rscript -e \"remotes::install_version('rjson', quiet=FALSE, upgrade='never', force=TRUE, dependencies=FALSE, repos='https://cloud.r-project.org', lib='{r_libs_user_directory}')\"",
+                f"Rscript -e \"remotes::install_version('omopgenerics', version='0.2.1', quiet=FALSE, upgrade='never', force=TRUE, dependencies=FALSE, repos='https://cloud.r-project.org', lib='{r_libs_user_directory}')\"",
+                f"Rscript -e \"remotes::install_version('CDMConnector', version='1.4.0', quiet=FALSE, upgrade='never', force=TRUE, dependencies=TRUE, repos='https://cloud.r-project.org', lib='{r_libs_user_directory}')\"",
+                f"Rscript -e \"remotes::install_version('PatientProfiles', quiet=FALSE, upgrade='never', force=TRUE, dependencies=TRUE, repos='https://cloud.r-project.org', lib='{r_libs_user_directory}')\"",
+                f"Rscript -e \"remotes::install_version('CohortSurvival', version='0.5.1', quiet=FALSE, upgrade='never', force=TRUE, dependencies=FALSE, repos='https://cloud.r-project.org', lib='{r_libs_user_directory}')\"",
+            ]
+        ).run()
+    else:
+        raise ValueError("Environment variable: 'R_LIBS_USER' is empty.")
+
+
+@flow(log_prints=True, persist_result=True, task_runner=SequentialTaskRunner)
+def cohort_survival_plugin(options: cohortSurvivalOptionsType):
+    setup_plugin()
+    # Cohort Gen has to be imported dynamically as dataflow-mgmt does not have flow source code files and prefect is validating file imports during creation of prefect deployments
+    cohort_survival_flow_module = importlib.import_module("flows.cohort_survival.flow")
+    cohort_survival_flow_module.execute_cohort_survival(options)
