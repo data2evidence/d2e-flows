2e9e6d434f9faa1edee37656012e587d5b519b5d
alicia-koh <alicia.koh@data4life-asia.care>
2024-07-30 17:58:41 +0800

    Remove duckdb in install requires

--- duckdb/setup.py
+++ duckdb/setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=["prefect==2.14.6", "s3fs==2023.1.0",
-                      "aiobotocore==2.4.2", "botocore==1.27.59", "duckdb==1.0"],
+                      "aiobotocore==2.4.2", "botocore==1.27.59"],
     include_package_data=True,
     data_files=[('metadata', ['metadata/alp-job.json'])],
 )
