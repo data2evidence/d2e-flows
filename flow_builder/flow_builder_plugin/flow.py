13a0eac8bb7bcb4d523e7db3f458c9d65ae255d8
santan maddi <saisantan2004@gmail.com>
2024-08-07 17:13:26 +0800

    flow-builder can download the flow, create docker image and prefect deployment

--- flow_builder/flow_builder_plugin/flow.py
+++ flow_builder/flow_builder_plugin/flow.py
@@ -1,19 +1,68 @@
+import json
+import os
+import subprocess
 import sys
-from prefect import flow
+import shutil
+from pathlib import Path
+from prefect import flow, get_run_logger, deploy
+from prefect.utilities.dockerutils import build_image
+from prefect.deployments.runner import RunnerDeployment
+
+def download_package(package_spec):
+    """
+    Download a package from a Git URL or npm registry to a specific directory using pnpm.
+
+    Args:
+        package_spec (str): The package specification, e.g. "github:user/repo#branch" or "@scope/pkg"
+        dest_dir (str): The destination directory to download the package source code
+    """
+    try:
+        subprocess.run(["pnpm", "add", package_spec], check=True)
+    except Exception as e:
+        get_run_logger(e)
 
 @flow
-def build_flow():
-    # install requirements.txt
-    my_flow = flow.from_source(
-        source="https://github.com/PrefectHQ/prefect.git",
-        entrypoint="flows/hello_world.py:hello"
-    )
-    my_flow.deploy(
-        name="hello_world", 
-        work_pool_name="test-docker-pool", 
-        image="hello_world:develop",
-        push=False
-    )
+def build_flow(name, url):
+    plugin_path = f'/tmp/node_modules/{name}'
+    ori_cwd = os.getcwd()
+    logger = get_run_logger()
+    os.chdir("/tmp")
+    try:
+        download_package(url)
+        os.chdir(plugin_path)
+
+        package_json_obj = {}
+        with open('package.json', 'r') as package_json_file:
+            package_json_obj = json.loads(package_json_file.read())
+        
+        logger.info(msg=json.dumps(package_json_obj))
+        config = package_json_obj.get('config', {})
+        plugin_version = package_json_obj.get('version', '0.1.0')
+        flow_name = config.get("name")
+        flow_type = config.get("type")
+        flow_params = config.get("params", None)
+        flow_entrypoint = config.get("entrypoint")
+        platform = package_json_obj.get('platform', 'linux/amd64')
+
+        os.chdir(ori_cwd)
+        build_image(Path(plugin_path), platform=platform, tag=f"{flow_name}:{plugin_version}", 
+                    stream_progress_to=sys.stdout, pull=True)
+        deploy(
+            RunnerDeployment.from_entrypoint(
+                entrypoint=f"{plugin_path}/{flow_entrypoint}", 
+                name=f"{flow_name}",
+                parameters=flow_params,
+                job_variables={"image_pull_policy": "Never"}
+            ),
+            work_pool_name="docker-pool", 
+            image=f"{flow_name}:{plugin_version}",
+            push=False,
+            build=False
+        )
+
+    finally:
+        logger.info(f'removing {plugin_path}')
+        shutil.rmtree('/tmp/node_modules')
 
 if __name__ == '__main__':
     kwargs = {}
@@ -21,13 +70,13 @@ if __name__ == '__main__':
         key, value = arg.split('=')
         kwargs[key] = value
 
-    sourcePath = kwargs.get('sourcePath')
-    entrypoint = kwargs.get('entrypoint')
-    build_flow.from_source(
-        sourcePath,
-        entrypoint
-    ).deploy(
-        name="flow_builder-deployment",
-        build=False,
-        push=False
-    )
+    cmd = kwargs.get('cmd', 'deploy')
+    match cmd:
+        case 'deploy':
+            build_flow.deploy(
+                name="flow_builder-deployment",
+                build=False,
+                push=False
+            )
+        case 'test':
+            build_flow('npm-flow-plugin', 'git+https://<PAT_TOKEN>:x-oauth-basic@github.com/alp-os/<REPO_NAME>#path:<DIR_PATH_OF_FLOW>')
