4fe7fcec373de3708f2ed0f17606a0f51a890ee9
Brandan <brandan.tanck@gmail.com>
2024-08-13 13:19:35 +0800

    rename duckdb references to cachedb

--- cachedb/setup.py
+++ cachedb/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='create_duckdb_file_plugin',
+    name='create_cachedb_file_plugin',
     version='0.1.0',
     packages=find_packages(),
     install_requires=["prefect==2.14.6", "s3fs==2023.1.0", "prefect-shell==0.2.2",
