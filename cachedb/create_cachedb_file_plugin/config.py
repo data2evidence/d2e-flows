4566b81537aff81c3d59d4866409661481e9f689
alicia-koh <alicia.koh@data4life-asia.care>
2024-08-26 13:50:33 +0800

    Fix merge conflict for cachedb plugin

--- cachedb/create_cachedb_file_plugin/config.py
+++ cachedb/create_cachedb_file_plugin/config.py
@@ -1,10 +1,14 @@
 from pydantic import BaseModel
-from typing import Any
+from typing import Any, Optional
 
 
 class CreateDuckdbDatabaseFileType(BaseModel):
     databaseCode: str
     schemaName: str
+    
+    # Flag used for cdw-config to create empty duckdb database file for validation
+    # When this flag is set to True, it will also save the duckdb database file into a separate volume only for cdw-svc
+    createForCdwConfigValidation: Optional[bool] = False
 
     @property
     def use_cache_db(self) -> str:
--- cachedb/create_cachedb_file_plugin/create.py
+++ cachedb/create_cachedb_file_plugin/create.py
@@ -23,7 +23,8 @@ def create_duckdb_database_file(options: CreateDuckdbDatabaseFileType, modules: 
     database_code = options.databaseCode
     schema_name = options.schemaName
     use_cache_db = options.use_cache_db
-    duckdb_database_name = f"{database_code}_{schema_name}"
+    create_for_cdw_config_validation = options.createForCdwConfigValidation
+    duckdb_database_name = "cdw_config_svc_validation" if create_for_cdw_config_validation else f"{database_code}_{schema_name}"
     
     dbdao = modules.dao_DBDao.DBDao(use_cache_db=use_cache_db,
                                     database_code=database_code,
@@ -38,17 +39,22 @@ def create_duckdb_database_file(options: CreateDuckdbDatabaseFileType, modules: 
         logger.error(error_message)
         raise ValueError(error_message)
 
+    # If file already exists, delete first before moving to copy step
+    _remove_existing_file_if_exists(
+        duckdb_database_name, create_for_cdw_config_validation)
+
     # TODO: Add switch case after unifiying envConverter postgres dialect value
-    copy_postgres_to_duckdb(dbdao, duckdb_database_name)
+    copy_postgres_to_duckdb(dbdao, duckdb_database_name, create_for_cdw_config_validation)
     logger.info(f"""Duckdb database file: {
                 duckdb_database_name} has been successfully created.""")
 
 
-def copy_postgres_to_duckdb(dbdao, duckdb_database_name: str):
+def copy_postgres_to_duckdb(dbdao, duckdb_database_name: str, create_for_cdw_config_validation: bool):
     logger = get_run_logger()
     
     # Get table names from db
-    table_names = dbdao.get_table_names()
+    # Include views when creating duckdb file for cdw config validation
+    table_names = dbdao.get_table_names(include_views=create_for_cdw_config_validation)
 
     # Get credentials for database code
     tenant_configs = dbdao.get_tenant_configs(dbdao.schema_name)
@@ -57,10 +63,18 @@ def copy_postgres_to_duckdb(dbdao, duckdb_database_name: str):
     for table in table_names:
         try:
             logger.info(f"Copying table: {table} from postgres into duckdb...")
+            
+            duckdb_file_path = _resolve_duckdb_file_path(
+                duckdb_database_name, create_for_cdw_config_validation)
+            
             with duckdb.connect(f"{os.getenv('DUCKDB__DATA_FOLDER')}/{duckdb_database_name}") as con:
+                
+                # If create_for_cdw_config_validation is True, add a LIMIT 0 to select statement so that only an empty table is created
+                limit_statement = "LIMIT 0" if create_for_cdw_config_validation else ""
+                
                 result = con.execute(
-                    f"""CREATE TABLE {duckdb_database_name}.{table} AS FROM (SELECT * FROM postgres_scan('host={tenant_configs['host']} port={tenant_configs['port']} dbname={
-                        tenant_configs['databaseName']} user={tenant_configs['user']} password={tenant_configs['password']}', '{dbdao.schema_name}', '{table}'))"""
+                    f"""CREATE TABLE {duckdb_database_name}."{table}" AS FROM (SELECT * FROM postgres_scan('host={tenant_configs['host']} port={tenant_configs['port']} dbname={
+                        tenant_configs['databaseName']} user={tenant_configs['user']} password={tenant_configs['password']}', '{dbdao.schema_name}', '{table}'){limit_statement})"""
                 ).fetchone()
                 logger.info(f"{result[0]} rows copied")
         except Exception as err:
@@ -68,6 +82,24 @@ def copy_postgres_to_duckdb(dbdao, duckdb_database_name: str):
             raise (err)
     logger.info("Postgres tables succesfully copied into duckdb database file")
 
+def _remove_existing_file_if_exists(duckdb_database_name: str, create_for_cdw_config_validation: bool):
+    logger = get_run_logger()
+    duckdb_file_path = _resolve_duckdb_file_path(
+        duckdb_database_name, create_for_cdw_config_validation)
+    if os.path.isfile(duckdb_file_path):
+        logger.info(f"Removing existing duckdb file at {duckdb_file_path}")
+        os.remove(duckdb_file_path)
+
+
+def _resolve_duckdb_file_path(duckdb_database_name: str, create_for_cdw_config_validation: bool):
+    '''
+    Gets duckdb data folder based on create_for_cdw_config_validation flag
+    '''
+    if create_for_cdw_config_validation:
+        # Returns a hardcoded file path when creating duckdb file for cdw_config validation
+        return f"{os.getenv('CDW_CONFIG_DUCKDB__DATA_FOLDER')}/{duckdb_database_name}"
+    else:
+        return f"{os.getenv('DUCKDB__DATA_FOLDER')}/{duckdb_database_name}"
 
 if __name__ == '__main__':
     database_code = "alpdev_pg"
