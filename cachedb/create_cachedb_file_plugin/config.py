a609ec943f5733e8577c64245f21b337a5d25d13
Brandan <56997231+brandantck@users.noreply.github.com>
2024-08-30 10:36:25 +0800

    [Analyze-1045] Add creation of duckdb fulltext search indexes (#51)
    * add creation of duckdb FTS indexes
    
    * update fts index settings
    
    * remove fts creation for concept_relationship and concept_ancestor
    
    * add note table
    
    * update log statement
    
    * update duckdb fts to use english for stemmer and stopwords

--- cachedb/create_cachedb_file_plugin/config.py
+++ cachedb/create_cachedb_file_plugin/config.py
@@ -16,3 +16,45 @@ class CreateDuckdbDatabaseFileModules(BaseModel):
     utils_types: Any
     dbutils: Any
     dao_DBDao: Any
+
+
+DUCKDB_FULLTEXT_SEARCH_CONFIG = {
+    "concept": {
+        "document_identifier": "concept_id",
+    },
+    # TODO: Temp commented out due to OOM error. Discuss if table is required or not.
+    # https://github.com/alp-os/internal/issues/1064
+    # "concept_relationship": {
+    #     # primary key does not exist in concept_relationship table
+    #     "document_identifier": "fts_document_identifier_id",
+    # },
+    "relationship": {
+        "document_identifier": "relationship_id",
+    },
+    "vocabulary": {
+        "document_identifier": "vocabulary_id",
+    },
+    "concept_synonym": {
+        # primary key does not exist in concept_synonym table,
+        "document_identifier": "fts_document_identifier_id",
+    },
+    "concept_class": {
+        "document_identifier": "concept_class_id",
+    },
+    "domain": {
+        "document_identifier": "domain_id",
+    },
+    # TODO: Temp commented out due to OOM error. Discuss if table is required or not.
+    # https://github.com/alp-os/internal/issues/1064
+    # "concept_ancestor": {
+    #     # primary key does not exist in concept_ancestor table
+    #     "document_identifier": "fts_document_identifier_id",
+    # },
+    "concept_recommended": {
+        # primary key does not exist in concept_ancestor table
+        "document_identifier": "fts_document_identifier_id",
+    },
+    "note": {
+        "document_identifier": "note_id",
+    },
+}
---
+++ cachedb/create_cachedb_file_plugin/util.py
@@ -0,0 +1,22 @@
+import os
+from prefect import get_run_logger
+
+
+def resolve_duckdb_file_path(duckdb_database_name: str, create_for_cdw_config_validation: bool):
+    '''
+    Gets duckdb data folder based on create_for_cdw_config_validation flag
+    '''
+    if create_for_cdw_config_validation:
+        # Returns a hardcoded file path when creating duckdb file for cdw_config validation
+        return f"{os.getenv('CDW_CONFIG_DUCKDB__DATA_FOLDER')}/{duckdb_database_name}"
+    else:
+        return f"{os.getenv('DUCKDB__DATA_FOLDER')}/{duckdb_database_name}"
+
+
+def remove_existing_file_if_exists(duckdb_database_name: str, create_for_cdw_config_validation: bool):
+    logger = get_run_logger()
+    duckdb_file_path = resolve_duckdb_file_path(
+        duckdb_database_name, create_for_cdw_config_validation)
+    if os.path.isfile(duckdb_file_path):
+        logger.info(f"Removing existing duckdb file at {duckdb_file_path}")
+        os.remove(duckdb_file_path)
