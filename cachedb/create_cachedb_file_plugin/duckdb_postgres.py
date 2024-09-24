6e1e6f2b5d621198c8b9085d0c1ec7273c31ed34
alicia-koh <alicia.koh@data4life-asia.care>
2024-09-24 17:31:17 +0800

    Create indices for duckdb

--- cachedb/create_cachedb_file_plugin/duckdb_postgres.py
+++ cachedb/create_cachedb_file_plugin/duckdb_postgres.py
@@ -3,6 +3,18 @@ from prefect import get_run_logger
 from create_cachedb_file_plugin.util import resolve_duckdb_file_path
 
 
+def create_table_indices(duckdb_file_path: str, duckdb_database_name: str, logger):
+    with duckdb.connect(duckdb_file_path) as con:
+        # Read the indices file
+        with open('./create_cachedb_file_plugin/duckdb_indices.sql', 'r') as file:
+            # Read SQL Script from file
+            create_indices_script = file.read()
+            logger.info(f"Creating indices for duckdb at {duckdb_database_name}..")
+            con.execute(f"""SET session search_path = '{duckdb_database_name}'""")
+            con.execute(create_indices_script)
+            logger.info(f"All indices successfully created")
+            
+
 def copy_postgres_to_duckdb(db_dao: any, duckdb_database_name: str, create_for_cdw_config_validation: bool):
     logger = get_run_logger()
 
@@ -12,14 +24,14 @@ def copy_postgres_to_duckdb(db_dao: any, duckdb_database_name: str, create_for_c
 
     # Get credentials for database code
     db_credentials = db_dao.tenant_configs
+    
+    duckdb_file_path = resolve_duckdb_file_path(duckdb_database_name, create_for_cdw_config_validation)
 
     # copy tables from postgres into duckdb
     for table in table_names:
         try:
             logger.info(f"Copying table: {table} from postgres into duckdb...")
 
-            duckdb_file_path = resolve_duckdb_file_path(
-                duckdb_database_name, create_for_cdw_config_validation)
             with duckdb.connect(duckdb_file_path) as con:
 
                 # If create_for_cdw_config_validation is True, add a LIMIT 0 to select statement so that only an empty table is created
@@ -28,11 +40,20 @@ def copy_postgres_to_duckdb(db_dao: any, duckdb_database_name: str, create_for_c
                 result = con.execute(
                     f"""CREATE TABLE {duckdb_database_name}."{table}" AS FROM (SELECT * FROM postgres_scan('host={db_credentials['host']} port={db_credentials['port']} dbname={
                         db_credentials['databaseName']} user={db_credentials['user']} password={db_credentials['password']}', '{db_dao.schema_name}', '{table}') {limit_statement})"""
-                ).fetchone()
+                ).fetchone()                
+                
                 logger.info(f"{result[0]} rows copied")
         except Exception as err:
-            logger.error(f"Table:{table} loading failed with error: {err}f")
+            logger.error(f"Table:{table} loading failed with error: {err}")
             raise (err)
+        
     logger.info("Postgres tables succesfully copied into duckdb database file")
+
+    try:
+        create_table_indices(duckdb_file_path, duckdb_database_name, logger)
+    except Exception as err:
+        logger.error(f"Failed to create indexes: {err}")
+        raise (err)
+    
     logger.info(
         f"""Duckdb database file: {duckdb_database_name} successfully created.""")
