07b45830e617df7bd77142e5884dcd2359f1cf19
alicia-koh <alicia.koh@data4life-asia.care>
2024-10-04 08:05:09 +0800

    Rework i2b2 data model creation flow
    *Remove async
    *Group tasks in a parent task
    *Move create schema to top
    *Add check to validate tables are created

--- flows/i2b2_plugin/utils.py
+++ flows/i2b2_plugin/utils.py
@@ -7,27 +7,37 @@ def get_version_from_tag(tag: str) -> str:
 def path_to_ant(tag: str) -> str:
     return f"i2b2-data-{tag[1:]}/edu.harvard.i2b2.data/Release_{get_version_from_tag(tag)}"
 
-async def download_source_code(tag_name: str):
-    await ShellOperation(
+def check_table_creation(dbdao):
+    '''
+    Check if tables were created
+    '''
+    table_list = dbdao.get_table_names()
+    if table_list == []:
+        raise Exception(f"Failed to create i2b2 tables. Schema is empty.")
+
+def download_source_code(tag_name: str):
+    ShellOperation(
         commands=[
             f"wget https://github.com/i2b2/i2b2-data/archive/refs/tags/{tag_name}.tar.gz",
-        ]).run()
+        ],
+        stream_output=False).run()
     
-async def unzip_source_code(tag_name: str):
-    await ShellOperation(
+def unzip_source_code(tag_name: str):
+    ShellOperation(
         commands=[         
-            f"tar -xzf {tag_name}.tar.gz"
-        ]).run()
+            f"tar -xzf {tag_name}.tar.gz",
+        ],
+        stream_output=True).run()
 
 
-async def setup_apache_ant(tag_name: str):
+def setup_apache_ant(tag_name: str):
     cwd = os.getcwd()
     ant_bin_dir = os.path.join(cwd, f"{path_to_ant(tag_name)}/apache-ant")
     
     # Set ant_home environment variable
     os.environ["ANT_HOME"] = ant_bin_dir
     
-    await ShellOperation(
+    ShellOperation(
         commands=[         
             f'ln -sfn {ant_bin_dir} /opt/ant',
             f'ln -sfn /opt/ant/bin/ant /usr/bin/ant',
