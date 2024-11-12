232eb319d179ea83b33e54ebcebe5fa8df4c49dc
Zhimin-arya <zhi.min@data4life-asia.care>
2024-11-12 17:33:49 +0800

    change the default value of "note_nlp_table" from "note" to "note_nlp"

--- flows/ner_extract_plugin/types.py
+++ flows/ner_extract_plugin/types.py
@@ -7,7 +7,7 @@ class NerExtractOptions(BaseModel):
     database_code: str
     schema_name: str
     note_table: Optional[str] = 'note'
-    note_nlp_table: Optional[str] = 'note'
+    note_nlp_table: Optional[str] = 'note_nlp'
 
     @property
     def use_cache_db(self) -> str:
