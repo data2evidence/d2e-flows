38ce00984ea3f57a5c001bbbcd8b7ea79b93f748
alicia-koh <alicia.koh@data4life-asia.care>
2024-12-23 09:18:18 +0800

    Change get_auth_token_from_input to prefect task

--- shared_utils/api/PrefectAPI.py
+++ shared_utils/api/PrefectAPI.py
@@ -1,5 +1,13 @@
+from prefect import task
+
 from shared_utils.types import AuthToken
 
-async def get_auth_token_from_input():
-    iter = AuthToken.receive(key_prefix="authtoken", timeout=30, poll_interval=3)
-    return await iter.next()
+
+@task(log_prints=True)
+def get_auth_token_from_input() -> AuthToken:
+    iter = AuthToken.receive(key_prefix="authtoken", timeout=300, poll_interval=3)
+    return iter.next()
+
+
+def get_token_value(auth_token: AuthToken) -> str:
+    return auth_token.token.get_secret_value().replace("Bearer ", "")
