017396a73b85dcd9ae006b858dab6de6ee6a1b80
alicia-koh <alicia.koh@data4life-asia.care>
2025-01-03 11:21:14 +0800

    Migrate to pydantic v2

--- flows/data_management_plugin/types.py
+++ flows/data_management_plugin/types.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from datetime import datetime
 from typing import List, Dict, Optional
-from pydantic import BaseModel, Field, UUID4, root_validator
+from pydantic import BaseModel, Field, UUID4, model_validator
 
 FLOW_NAME = "data_management_plugin"
 
@@ -50,7 +50,7 @@ class DataModelType(BaseModel):
     def changelog_filepath_list(self) -> Dict:
         return DATAMODEL_CHANGELOG_MAPPING
     
-    @root_validator(pre=True)
+    @model_validator(mode='before')
     def set_default_vocab_schema(cls, values):
         if values.get('vocab_schema') is None:
             values['vocab_schema'] = values.get('schema_name')
