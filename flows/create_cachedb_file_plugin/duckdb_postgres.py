6240cc70a983f211d957fa4fd5c553dfcc4fcc4c
alicia-koh <110385419+alicia-koh@users.noreply.github.com>
2025-01-07 09:49:45 +0800

    [data-1373] Create index based on existing indexes in db table (#117)
    * Create index based on sqlalchemy indexes
    
    * Remove sql script
    * Add method in sqlalchemydao to retrieve indexes for a table
    * Add creation of indexes on primary key

--- flows/create_cachedb_file_plugin/duckdb_postgres.py
+++ flows/create_cachedb_file_plugin/duckdb_postgres.py
@@ -5,17 +5,6 @@ from prefect.logging import get_run_logger
 
 from flows.create_cachedb_file_plugin.utils import resolve_duckdb_file_path, DUCKDB_EXTENSIONS_FILEPATH
 
-def create_table_indices(duckdb_file_path: str, duckdb_database_name: str, logger):
-    with duckdb.connect(duckdb_file_path) as con:
-        # Read the indices file
-        with open('./flows/create_cachedb_file_plugin/duckdb_indices.sql', 'r') as file:
-            # Read SQL Script from file
-            create_indices_script = file.read()
-            logger.info(f"Creating indices for duckdb at {duckdb_database_name}..")
-            con.execute(f"""SET session search_path = '{duckdb_database_name}'""")
-            con.execute(create_indices_script)
-            logger.info(f"All indices successfully created")
-
 
 @task(log_prints=True)
 def copy_postgres_to_duckdb(db_dao: any, duckdb_database_name: str, create_for_cdw_config_validation: bool):
@@ -42,22 +31,44 @@ def copy_postgres_to_duckdb(db_dao: any, duckdb_database_name: str, create_for_c
                 # If create_for_cdw_config_validation is True, add a LIMIT 0 to select statement so that only an empty table is created
                 limit_statement = "LIMIT 0" if create_for_cdw_config_validation else ""
 
+                # Copy table and data
                 result = con.execute(
                     f"""CREATE TABLE {duckdb_database_name}."{table}" AS FROM (SELECT * FROM postgres_scan('host={db_credentials.host} port={db_credentials.port} dbname={
                         db_credentials.databaseName} user={db_credentials.readUser} password={db_credentials.readPassword.get_secret_value()}', '{db_dao.schema_name}', '{table}') {limit_statement})"""
                 ).fetchone()
                 logger.info(f"{result[0]} rows copied")
+
+                # Create index based on index in db table
+                indexes = db_dao.get_indexes_for_table(table)
+
+                for index in indexes:
+                    index_name = index.get("name")
+                    column_names = index.get("column_names")
+                    columns_str = ', '.join(column_names)
+                    unique = index.get("unique")
+
+                    # by default indexes created on columns in asc order
+                    if unique:
+                        index_query = f"CREATE UNIQUE INDEX {index_name} ON {table} ({columns_str})"
+                    else:
+                        index_query = f"CREATE INDEX {index_name} ON {table} ({columns_str})"
+                    
+                    logger.info(f"Running query: {index_query}") 
+                    con.execute(index_query)
+
+                pk_index = db_dao.get_indexes_for_pk(table)
+                pk_index_name = pk_index.get("name")
+                pk_index_columns = pk_index.get("constrained_columns")
+                
+                if pk_index_name is not None and pk_index_columns != []:
+                    pk_index_query = f"CREATE UNIQUE INDEX {pk_index_name} ON {table} ({', '.join(pk_index_columns)})"
+                    logger.info(f"Running query: {pk_index_query}") 
+                    con.execute(pk_index_query)
+
         except Exception as err:
-            logger.error(f"Table:{table} loading failed with error: {err}f")
+            logger.error(f"Table and index creation for table '{table}' failed with error: {err}f")
             raise (err)
     logger.info("Postgres tables succesfully copied into duckdb database file")
-    
-
-    try:
-        create_table_indices(duckdb_file_path, duckdb_database_name, logger)
-    except Exception as err:
-        logger.error(f"Failed to create indexes: {err}")
-        raise (err)
 
     logger.info(
         f"""Duckdb database file: {duckdb_database_name} successfully created.""")
